fc74c15d67e7993ac1544636dcab9f1f18cbe7de
Nishkarsh Saxena <nishkarsh.saxena03@gmail.com>
2022-02-01 09:41:01 +0530

    Edit Item functionality implemented

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ function App() {
   const [currentInput, setCurrentInput] = useState("");
   const [itemList, setItemList] = useState([]);
   const [itemCreatedCount, setItemCreatedCount] = useState(1);
+  // Editing States
+  const [editMode, setEditMode] = useState(false);
+  const [item2EditId, setItem2EditId] = useState(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    if(!editMode){
     console.log("Item submitted: " + currentInput);
     setItemList([...itemList, { text: currentInput, id: itemCreatedCount }])
-    setItemCreatedCount(preVal => preVal+1);
+    setItemCreatedCount(preVal => preVal + 1);
+    }
+    else{
+      const newItemList = itemList.map((item) => {
+        if(item.id === item2EditId){
+          return {...item, text: currentInput}
+        }
+        return item;
+      })
+      setItemList(newItemList);
+      setEditMode(false);
+      setItem2EditId(null);
+    }
     setCurrentInput("");
+  }
+
+  const editItemWithId = (id) => {
+    setEditMode(true);
+    const item2Edit = itemList.find((item) => item.id === id);
+    setItem2EditId(item2Edit.id);
+    setCurrentInput(item2Edit.text);
   }
 
   return (
@@ -20,15 +43,19 @@ function App() {
       <h2>Grocery Bud</h2>
       <form className='form' onSubmit={submitHandler}>
         <div className='form-control'>
-          <input id='itemToAdd' 
-          type='text' 
-          value={currentInput}
-          onChange={(e) => setCurrentInput(e.target.value)}
+          <input id='itemToAdd'
+            type='text'
+            value={currentInput}
+            onChange={(e) => setCurrentInput(e.target.value)}
           />
           <button>Submit</button>
         </div>
       </form>
-      <List items={itemList} />
+
+      <div className='grocery-container'>
+        <List items={itemList} editItem={editItemWithId} />
+      </div>
+
     </section>
   )
 }
--- src/List.js
+++ src/List.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-function List({items}){
+function List({ items, editItem }) {
 
   const listItems = items.map((item) => {
-    return <div key={item.id}>{item.text}</div>
+    return (
+      <div className='grocery-item'>
+        <p key={item.id}>{item.text}</p>
+        <button onClick={() => editItem(item.id)}><FaEdit /></button>
+      </div>
+    )
+
   })
 
   return (
-    <div>{listItems}</div>
-    
+    <div className='grocery-list'>{listItems}</div>
   )
 }
 
