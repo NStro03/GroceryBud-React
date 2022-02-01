884593626f90fe257e86c2c935bc05cc683d80ea
Nishkarsh Saxena <nishkarsh.saxena03@gmail.com>
2022-02-01 08:23:27 +0530

    Add functionality of Dynamic List implemented

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [currentInput, setCurrentInput] = useState("");
+  const [itemList, setItemList] = useState([]);
+  const [itemCreatedCount, setItemCreatedCount] = useState(1);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Item submitted: " + currentInput);
+    setItemList([...itemList, { text: currentInput, id: itemCreatedCount }])
+    setItemCreatedCount(preVal => preVal+1);
+    setCurrentInput("");
+  }
+
+  return (
+    <section className='section-center'>
+      <h2>Grocery Bud</h2>
+      <form className='form' onSubmit={submitHandler}>
+        <div className='form-control'>
+          <input id='itemToAdd' 
+          type='text' 
+          value={currentInput}
+          onChange={(e) => setCurrentInput(e.target.value)}
+          />
+          <button>Submit</button>
+        </div>
+      </form>
+      <List items={itemList} />
+    </section>
+  )
 }
 
 export default App
--- src/List.js
+++ src/List.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+function List({items}){
+
+  const listItems = items.map((item) => {
+    return <div key={item.id}>{item.text}</div>
+  })
+
+  return (
+    <div>{listItems}</div>
+    
+  )
 }
 
 export default List
